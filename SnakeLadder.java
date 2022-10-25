79a786e2b04ce88f825a38aefdeaaaa93a1bd5db
Sunit Ajgar <109612599+SunitAjgar@users.noreply.github.com>
2022-10-26 01:41:10 +0530

    Displaying Snake and Ladder game

---
+++ SnakeLadder.java
@@ -0,0 +1,9 @@
+package com.blz.assignment.day4;
+
+import java.util.Random;
+
+public class SnakeLadder {
+	public static void main(String[] args) {
+	System.out.println("Displaying Snake and Ladder game");
+	}
+}
