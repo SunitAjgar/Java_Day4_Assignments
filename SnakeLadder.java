2b728da95e0657b55af507d8c54df0290c6677b1
Sunit Ajgar <109612599+SunitAjgar@users.noreply.github.com>
2022-10-26 01:52:24 +0530

    Repeating till the Player reaches the winning position 100.

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -4,37 +4,38 @@ import java.util.Random;
 
 public class SnakeLadder {
 	public static void main(String[] args) {
-	System.out.println("Displaying Snake and Ladder game");
-	int position = 0;
-	int count = 0;
-	Random random = new Random();
-	int randomNum = random.nextInt(6) + 1;
-	System.out.println("Dice generated " +randomNum);
-	int option = random.nextInt(3);
-	System.out.println("option = " +option);
-	switch (option) {
-	case 0:
-		System.out.println("Player stays in the same position");
-		break;
-	case 1:
-		position = position + randomNum;  
-		
-		if (position > 100)
-		{
-		   position -= randomNum;
+		System.out.println("Displaying Snake and Ladder game");
+		int position = 0;
+		int count = 0;
+		Random random = new Random();
+		int randomNum = random.nextInt(6) + 1;
+		System.out.println("Dice generated " + randomNum);
+		int option = random.nextInt(3);
+		System.out.println("option = " + option);
+		switch (option) {
+
+		case 0:
+			System.out.println("Player stays in the same position");
+			break;
+
+		case 1:
+			position = position + randomNum;
+			if (position > 100)
+			{
+			   position -= randomNum;
+			}
+			System.out.println("Player moved at next position " + position);
+			break;
+
+		case 2:
+			position -= randomNum;
+			if (position < 0)
+			{
+				position = 0;
+			}
+			System.out.println("Player moved back " + position);
+
 		}
-		System.out.println("Player moved at next position " +position);
-		break;
-	case 2:
-		position-= randomNum;
-		if (position < 0)
-		{
-			position = 0;
-		}
-		System.out.println("Player moved back " +position);
-		
-		
-	}
-	System.out.println();
+		System.out.println();
 	}
 }
