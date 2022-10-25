7a9dd6068497db997e50909d5c71627e5900171c
Sunit Ajgar <109612599+SunitAjgar@users.noreply.github.com>
2022-10-26 01:42:18 +0530

    Snake and Ladder game played with single player at start position 0

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -5,5 +5,7 @@ import java.util.Random;
 public class SnakeLadder {
 	public static void main(String[] args) {
 	System.out.println("Displaying Snake and Ladder game");
+	int position = 0;
+	int count = 0;
 	}
 }
